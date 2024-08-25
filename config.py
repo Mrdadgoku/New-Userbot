40d0d66acdf2ab6f46c4bf8c98acb5891fb3d57e
GOKU <dr084891@gmail.com>
2024-08-25 22:13:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", "12380656"))
-API_HASH = getenv("API_HASH", "d927c13beaaf5110f25c505b7c071273")
+API_ID = int(getenv("API_ID", "21415583"))
+API_HASH = getenv("API_HASH", "537124a5f4131e215aa2aefafae96db4")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = int(getenv("OWNER_ID", ""))
 STRING_SESSION = getenv("STRING_SESSION", "")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6812888267").split()))
-ALIVE_PIC = getenv("ALIVE_PIC", "https://graph.org/file/9db83cb6ae0b870c34e49.jpg")
-REPO_URL = getenv("REPO_URL", "https://github.com/Team-CF-Bots/Lily-Userbot")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7379512533").split()))
+ALIVE_PIC = getenv("ALIVE_PIC", "https://graph.org/file/ef7782ca71ddef38077ce.jpg")
+REPO_URL = getenv("REPO_URL", "https://github.com/Mrdadgoku/New-Userbot")
 BRANCH = getenv("BRANCH", "main")
